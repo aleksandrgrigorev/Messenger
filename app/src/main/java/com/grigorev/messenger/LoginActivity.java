2e16373afb23cbef9155546618c28f2898aec719
aleksandrgrigorev <xundr1992@gmail.com>
2023-08-03 15:52:34 +0400

    Login activity impl

---
+++ app/src/main/java/com/grigorev/messenger/LoginActivity.java
@@ -0,0 +1,44 @@
+package com.grigorev.messenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewForgotPassword;
+    private TextView textViewRegister;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initViews();
+        buttonLogin.setOnClickListener(view -> {
+            String email = editTextEmail.getText().toString().trim();
+            String password = editTextPassword.getText().toString().trim();
+            //login
+        });
+        textViewForgotPassword.setOnClickListener(view -> {
+            // launch forgot password screen intent
+        });
+        textViewRegister.setOnClickListener(view -> {
+            // launch register screen intent
+        });
+    }
+
+    private void initViews() {
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewRegister = findViewById(R.id.textViewRegister);
+    }
+}

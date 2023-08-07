df9aeb822595e8ad7c5358e8d2b2b7be900092a4
aleksandrgrigorev <xundr1992@gmail.com>
2023-08-07 17:20:55 +0400

    ChatActivity impl

---
+++ app/src/main/java/com/grigorev/messenger/MessagesAdapter.java
@@ -0,0 +1,16 @@
+package com.grigorev.messenger;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MessagesAdapter {
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
